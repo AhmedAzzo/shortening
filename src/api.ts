83ca1c9e11db940c8c6a4809ac803fcbe670f5d4
Ahmed Azzo <ahmed.azzo@penny.co>
2023-08-12 10:41:37 +0300

    finish initializing the base app structure

---
+++ src/api.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import healthCheck from '@components/healthcheck/healthCheck.router';
+
+
+const router: Router = Router();
+router.use(healthCheck);
+
+
+export default router;
---
+++ src/components/healthcheck/healthCheck.router.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import healthcheck from './healthCheck.controller';
+
+const router: Router = Router();
+router.get('/health', healthcheck);
+
+export default router;
